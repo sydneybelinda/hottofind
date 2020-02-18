df7e3f1226b501d4fe2296a88e19f9414ffd1958
Belinda Launer <launerb@gmail.com>
2020-02-18 23:39:05 +1100

    App Updated

--- pages/posts/[...slug].js
+++ pages/posts/[...slug].js
@@ -16,7 +16,6 @@ import { withAuth } from "../../utils/auth";
 import * as Queries from "../../utils/queries";
 import Sort from "../../components/sort";
 import cookies from "next-cookies";
-import Immutable from "immutable";
 
 const useStyles = makeStyles(theme => ({
   toolbar: {
