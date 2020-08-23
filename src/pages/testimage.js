1df5b93700102945153ae5c328ff9bd82f43eb58
Belinda Launer <launerb@gmail.com>
2020-08-23 13:18:58 +1000

    App Updated

---
+++ src/pages/testimage.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+
+export default class Main extends Component {
+	static async getInitialProps() {
+		return {
+		
+		};
+	}
+
+	render() {
+		return (
+            <div>
+<img src={require("/data/uploadedimages/noimage.jpg") } />
+</div>
+		);
+	}
+}
